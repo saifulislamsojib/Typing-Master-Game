c1db67bdcf218bbe1a2f866ea1049b3f37ad6c89
saifulislamsojib <saifulislam.sojib56@gmail.com>
2021-02-10 21:53:37 +0600

    finish

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 const url = 'https://api.quotable.io/random';
+const quoteDisplay = document.getElementById('quote-display');
+const quoteInput = document.getElementById('quote-input');
+const timerDisplay = document.getElementById('timer-display');
+
 
 // Get Random Quote
 const getRandomQuote = async () => {
@@ -10,6 +14,53 @@ getRandomQuote();
 
 // Display Random Quote
 const displayRandomQuote = (content) => {
-    const quoteDisplay = document.getElementById('quote-display');
-    quoteDisplay.innerText = content;
+    if (content.length < 150 && content.length > 40) {
+        quoteDisplay.innerHTML = "";
+        [...content].map(quote => {
+            const span = document.createElement('span');
+            span.innerText = quote;
+            quoteDisplay.appendChild(span);
+        })
+        quoteInput.value = null;
+    }
+    else{
+        getRandomQuote();
+    }
+    setTime();
+}
+
+// correct or incorrect
+quoteInput.addEventListener("input", e => {
+    const spans = document.querySelectorAll("span");
+    const arr = [...e.target.value];
+    let finish = true;
+    [...spans].map((span, i) =>{
+        if (arr[i] == null) {
+            span.classList.remove("incorrect");
+            span.classList.remove("correct");
+            finish = false;
+        }
+        else if (span.innerText === arr[i]) {
+            span.classList.add("correct");
+            span.classList.remove("incorrect");
+        }
+        else{
+            span.classList.add("incorrect");
+            span.classList.remove("correct");
+            finish = false;
+        }
+    })
+    if (finish) {
+        getRandomQuote();
+    }
+})
+
+// Set Timer
+const setTime = () => {
+    timerDisplay.innerHTML = 0;
+    const startDate = new Date;
+    setInterval(() => {
+        let second = Math.floor((new Date - startDate)/1000);
+        timerDisplay.innerHTML = second;
+    }, 1000);
 }
